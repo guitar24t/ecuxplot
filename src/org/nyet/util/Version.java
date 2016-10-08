872bb8898524b894fc24bb7f9e59e73d92601e8c
Robert Hilton <robert.a.hilton.jr@gmail.com>
2016-10-08 18:48:25 -0400

    Update version and add target for mapdump

--- src/org/nyet/util/Version.java
+++ src/org/nyet/util/Version.java
@@ -4,25 +4,23 @@ import java.util.ArrayList;
 
 public class Version extends ArrayList<Object> {
     private static final long serialVersionUID = 20150620L;
-    public static final String ECUxPlot="%ECUXPLOT_VER";
-    public static final String JFreeChart="%JFREECHART_VER";
-    public static final String JCommon="%JCOMMON_VER";
-    public static final String OpenCSV="%OPENCSV_VER";
-    public static final String JavacMajor = "%JAVAC_MAJOR_VER";
-    public static final String CommonsLang3 = "%COMMONS_LANG3_VER";
+    public static final String ECUxPlot = "v0.10r0.0";
+    public static final String JFreeChart = "1.0.19";
+    public static final String JCommon = "1.0.23";
+    public static final String OpenCSV = "3.8";
+    public static final String CommonsLang3 = "3.4";
 
-    public Version () {
-	this.add("ECUxPlot " + Version.ECUxPlot);
-	this.add("JFreeChart " + Version.JFreeChart);
-	this.add("JCommon " + Version.JCommon);
-	this.add("OpenCSV " + Version.OpenCSV);
-	this.add("commons-lang3 " + Version.CommonsLang3);
-	this.add("Java compiler " + Version.JavacMajor);
-	this.add("Java runtime "+ System.getProperty("java.version"));
+    public Version() {
+        this.add("ECUxPlot " + Version.ECUxPlot);
+        this.add("JFreeChart " + Version.JFreeChart);
+        this.add("JCommon " + Version.JCommon);
+        this.add("OpenCSV " + Version.OpenCSV);
+        this.add("commons-lang3 " + Version.CommonsLang3);
+        this.add("Java runtime " + System.getProperty("java.version"));
     }
 
     public String toString() {
-	String s = Strings.join("<br>", this.toArray());
-	return "<html>"+s+"</html>";
+        String s = Strings.join("<br>", this.toArray());
+        return "<html>" + s + "</html>";
     }
 }
